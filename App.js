3e2656a9357b7e9cdeb1ac073f8c90102a3bd353
loucims <sebastian.loucim@gmail.com>
2022-05-19 22:53:44 -0300

    Webhook integration test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenido a ReservEat!</Text>
+      <Text>Bienvenido a ReservEat!!</Text>
       <StatusBar style="auto" />
     </View>
   );
