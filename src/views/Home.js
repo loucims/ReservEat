82ec4ec3095360feaf1d93ce079c0f3d1118f543
Sebastian Loucim <sebastian.loucim@gmail.com>
2022-08-29 04:08:17 -0300

    Buttons working, consistent style

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,72 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 //----------------------------------------------------------\\
 import Map from '../components/Map';
 import { foodCategories } from '../utils/categories';
 import { colors } from '../utils/colors';
 import MainView from '../components/MainView'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+
+const map = require("../../assets/icons/statusBar/map.png")
+const mapPressed = require("../../assets/icons/statusBar/map-pressed.png")
+const user = require("../../assets/icons/statusBar/user.png")
+const userPressed = require("../../assets/icons/statusBar/user-pressed.png")
+const reserves = require("../../assets/icons/statusBar/reserves.png")
+const reservesPressed = require("../../assets/icons/statusBar/reserves-pressed.png")
 
 const HomeView = ({navigation}) => {
+    const [currentScreen, setCurrentScreen] = useState('Discovery');
+
     const [searchField, setSearchField] = useState('');
+
+    const RenderCurrentView = () => {
+        switch(currentScreen){
+            case 'User':
+                break
+            case 'Discovery':
+                break
+            case 'Reservations':
+                break   
+        }
+    }
 
 
     return(
-        <MainView statusColor={'light-content'} safeAreaTopColor={colors.red} safeAreaBottomColor={colors.red}>
+        <MainView statusColor={'light-content'} safeAreaTopColor={colors.red} safeAreaBottomColor={colors.red} direction={'column-reverse'}>
+
+            <View style={styles.statusBarView}>
+                <View style={styles.statusBar}>
+                    <TouchableOpacity onPress={() => {
+                        setCurrentScreen('User');
+                    }}>
+                        <Image source={currentScreen == 'User' ? userPressed : user} style={styles.icon}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        setCurrentScreen('Discovery');
+                    }}>
+                        <Image source={currentScreen == 'Discovery' ? mapPressed : map} style={styles.icon}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        setCurrentScreen('Reservations');
+                    }}>
+                        <Image source={currentScreen == 'Reservations' ? reservesPressed : reserves} style={styles.icon}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+
+            <Map navigation={navigation}/> 
+
+
             <View style={styles.discoveryBarView}>
                 <View style={styles.discoveryBar}>
-                    <Image source={require("../../assets/icons/discoveryBar/search.png")} style={styles.searchIcon} marginHorizontal={20} marginTop={15}/>
+                    <Image source={require("../../assets/icons/discoveryBar/search.png")} style={styles.searchIcon} marginHorizontal={'5%'} marginTop={'3%'}/>
                     <View>
                         <TextInput onChangeText={(input) =>{setSearchField(input)}} style={styles.input} 
                         fontSize={20} placeholder="Buscar.." placeholderTextColor={colors.white}/>
@@ -30,22 +81,10 @@ const HomeView = ({navigation}) => {
                     ))}
                 </ScrollView>
             </View>
-            <Map navigation={navigation}/> 
-            <View style={styles.statusBarView}>
-                <View style={styles.statusBar}>
-                    <TouchableOpacity>
-                        <Image source={require("../../assets/icons/statusBar/map-pressed.png")} style={styles.icon}/>
-                    </TouchableOpacity>
 
-                    <TouchableOpacity>
-                        <Image source={require("../../assets/icons/statusBar/reserves.png")} style={styles.icon}/>
-                    </TouchableOpacity>
 
-                    <TouchableOpacity>
-                        <Image source={require("../../assets/icons/statusBar/user.png")} style={styles.icon}/>
-                    </TouchableOpacity>
-                </View>
-            </View>
+
+            
         </MainView>
     );
 };
@@ -60,11 +99,11 @@ const styles = StyleSheet.create({
     discoveryBarView: {
         width: '100%',
         height: '13%',
-        backgroundColor: colors.black
+        backgroundColor: colors.red
     },
     discoveryBar: {
         flexDirection:'row',
-        marginTop: 10
+        marginTop: RFPercentage(0.1)
     },
     scrollDiscoveryBar: {
         width: '100%',
@@ -73,7 +112,7 @@ const styles = StyleSheet.create({
     categoryButton: {
         backgroundColor: colors.white,
         borderRadius: 50,
-        width: 100,
+        width: '20%',
         height: 30,
         marginHorizontal: 10,
         justifyContent: 'center',
@@ -84,19 +123,19 @@ const styles = StyleSheet.create({
         color: colors.black
     },
     statusBarView: {
-        backgroundColor: colors.black,
+        backgroundColor: colors.red,
         width: '100%',
         height: '10%'
     },
     statusBar: {
         flexDirection:'row',
         justifyContent: 'space-around',
-        alignItems: 'baseline',
-        marginTop: 25
+        alignItems: 'center',
+        height: '100%',
     },
     icon: {
-        width: 30,
-        height: 30
+        aspectRatio: 1,
+        height: RFValue(27),
     },
     searchIcon:{
         width: 24,
